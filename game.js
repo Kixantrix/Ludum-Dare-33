72ed7b522d42a31859bd8c35f96f5bfb4e0f3f09
Michael Von Hippel <vonhippelscienceandenterprises@gmail.com>
2015-08-21 21:32:00 -0700

    starfield gen

--- game.js
+++ game.js
@@ -78,6 +78,9 @@ function Background() {
     this.default_width = 2000;
     this.default_height = 2000;
 
+    this.starFields = {}
+
+
 
     this.draw = function(camera) {
 
@@ -87,15 +90,34 @@ function Background() {
 }
 
 // A field of stars as one chunk of width and height
-function StarField(width, height, numSmallStars, numBigStars) {
+function StarField(width, height, smallStarWidth, largeStarWidth, numSmallStars, numBigStars) {
+    // Arrays of stars
     this.bigStars = [];
     this.smallStars = [];
 
-    for(var i; i < numSmallStars; i++) {
+    // Width of the entire field
+    this.width = width;
+    this.height = height;
 
+    // Width of each kind of star
+    this.smallStarWidth = smallStarWidth;
+    this.largeStarWidth = largeStarWidth;
+
+    // Gen small stars coords
+    for(var i; i < numBigStars; i++) {
+        x = Math.floor(Math.Random() * width);
+        y = Math.floor(Math.Random() * height);
+        bigStars.add(new Point(x, y));
     }
 
-    for(var i; i < numSmallStars; i++) {
+    // Gen large stars coords
+    for(var i; i < numSmallStars; i++) {   
+        x = Math.floor(Math.Random() * width);
+        y = Math.floor(Math.Random() * height);
+        smallStars.add(new Point(x, y));
+    }
+    
+    this.draw = function() {
         
     }
 
