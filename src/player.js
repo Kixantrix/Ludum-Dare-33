412c4c0e590239779f0d8fbc6d7da703edac2177
Evan Davis <cptroot@gmial.com>
2015-08-21 22:01:00 -0700

    Using prototype stuff

--- src/player.js
+++ src/player.js
@@ -4,10 +4,11 @@ function Player(x, y) {
 
 	this.image = new Image();
 	this.image.src = "images/player_ship.png";
-	this.draw = function (ctx, camera) {
-		ctx.drawImage.apply(ctx, 
-			[this.image].concat(camera.transform(this.x, this.y)));
-	}
+}
+
+Player.prototype.draw = function (ctx, camera) {
+	ctx.drawImage.apply(ctx, 
+		[this.image].concat(camera.transform(this.x, this.y)));
 }
 
 module.exports = Player;
